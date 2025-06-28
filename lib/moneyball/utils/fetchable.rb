aad4c0401d64d03ddc266102cdec76fe6cf99940
Jose Victor Ferreira <root@josevictor.me>
2025-06-27 22:12:56 -0300

    fixed issue when no attribute, set default to the value of 1

--- lib/moneyball/utils/fetchable.rb
+++ lib/moneyball/utils/fetchable.rb
@@ -9,9 +9,10 @@ module Moneyball
 
       sig { params(player_data: T::Hash[String, String], attribute: String).returns(Integer) }
       def fetch_attribute(player_data, attribute)
-        attribute_str = player_data.fetch(attribute, '0').to_s
+        attribute_str = player_data.fetch(attribute, '1').to_s
         if attribute_str.include?('-')
-          (attribute_str.split('-').map(&:to_i).sum / 2.0).to_i
+          result = (attribute_str.split('-').map(&:to_i).sum / 2.0).to_i
+          result.zero? ? 1 : result
         else
           attribute_str.to_i
         end
